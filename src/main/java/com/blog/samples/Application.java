d802008b8053caf9f6af0828bfbc98d9cee8adba
Brian <hannawaybrian@googlemail.com>
2016-02-16 07:25:34 +0000

    removed annotations that are not required

--- src/main/java/com/blog/samples/Application.java
+++ src/main/java/com/blog/samples/Application.java
@@ -2,14 +2,8 @@ package com.blog.samples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = "com.blog.samples")
-//@EnableJpaRepositories(basePackages = "com.blog.samples.boot.rest.repository")
-//@EntityScan(basePackages = "com.blog.samples.boot.rest.model")
 public class Application {
     
     public static void main(String[] args) {
